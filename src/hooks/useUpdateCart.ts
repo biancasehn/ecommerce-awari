d27b331631eeec070eb4d4929696156a24bae274
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2022-04-12 17:44:08 +0200

    Improvement: Rewrites function for better readability

--- src/hooks/useUpdateCart.ts
+++ src/hooks/useUpdateCart.ts
@@ -5,11 +5,11 @@ import { getIdFromUrl } from "../utils/getIdFromUrl"
 export const useUpdateCart = (onSuccess: any) => {
     const { cartItems, addItemToCart, updateItemCount } = useStore();
     const existingItem = (id: number) => cartItems.filter((item) => item.id === id);
-
+    
     const addItem = (pokemon: any) => {
       const id = getIdFromUrl(pokemon.url);
       const hasItem = existingItem(id)
-
+      
       !hasItem.length
         ?  addItemToCart({
             ...pokemon,
@@ -19,18 +19,17 @@ export const useUpdateCart = (onSuccess: any) => {
             sprite: `${sprite}/${getIdFromUrl(pokemon.url)}.png`,
           })
         : updateItemCount(
-            cartItems.map((item) =>
-              item.id === id
-                ? {
-                    ...item,
-                    count: item.count + 1,
-                    price: item.price + 10,
-                  }
-                : item
-            )
+            cartItems.map((item) => {
+              if (item.id !== id) return item
+              return {
+                  ...item,
+                  count: item.count + 1,
+                  price: item.price + 10,
+                }
+            })
           );
-
-      !!onSuccess && onSuccess()
+      
+          !!onSuccess && onSuccess()
     }
     return {addItem}
 };
