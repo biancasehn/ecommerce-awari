8613912bdd353db6698862418850dead6d9cfd7a
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2022-04-13 18:42:57 +0200

    Adds cart icon and number of items in the cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import { Flex, Box, Image } from "@chakra-ui/react";
 import logo from "../assets/images/logo.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useStore } from "../services/store";
 
-function Header(props: any) {
+function Header() {
+  const { cartItems } = useStore();
+  const countCartItems = cartItems.reduce((acc, item) => {
+    return acc + item.count;
+  }, 0);
+
   return (
     <Box
-      p={4}
-      pr="5em"
-      pl="5em"
+      p="10px 50px"
       color="darkText"
       bgGradient="linear(90deg, header.orange 18%, header.lightYellow 50%, header.darkYellow  100%)"
       boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
@@ -16,13 +21,25 @@ function Header(props: any) {
         <Link to="/">
           <Image src={logo} w="8em" alt="logo" />
         </Link>
-        <Flex fontWeight="bold">
-          <Box p={3}>
-            <Link to="/">HOME</Link>
-          </Box>
-          <Box p={3}>
-            <Link to="/cart">CART</Link>
-          </Box>
+        <Flex p={3} position="relative">
+          <Link to="/cart">
+            <AiOutlineShoppingCart size="40px" />
+            <Box
+              width="20px"
+              height="20px"
+              position="absolute"
+              right="0"
+              bottom="0"
+              bg="button.green"
+              borderRadius="50%"
+              textAlign="center"
+              boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
+            >
+              <Box fontSize="14px" color="whiteText">
+                {countCartItems}
+              </Box>
+            </Box>
+          </Link>
         </Flex>
       </Flex>
     </Box>
