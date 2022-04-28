c2378910eef9187f9f6d8ae3a6941c2bc8b92fc8
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2022-04-28 18:01:21 +0200

    changed button theme's name and added theme for profile button

--- src/styles/components/ButtonStyles.ts
+++ src/styles/components/ButtonStyles.ts
@@ -11,11 +11,15 @@ export const ButtonStyles = {
       color: "button.white",
       _hover: { transform: "scale(1.05)" },
     },
-    login: {
+    submit: {
       fontWeight: "normal",
       backgroundColor: "button.submit",
       color: "button.white",
       borderRadius: "0",
+    },
+    profile: {
+      backgroundColor: "transparent",
+      _focus: "none",
     },
   },
   defaultProps: {},
