723492f725c55ef0c4c2dfff0f48c9f355369565
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2022-08-24 23:47:47 +0200

    improve: Search bar layout

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, Input } from "@chakra-ui/react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const SearchBar: React.FC<any> = () => {
+const SearchBar: React.FC<any> = ({ searchParam }) => {
   const [searchValue, setSearchValue] = useState("");
   let navigate = useNavigate();
   const { pathname } = useLocation();
@@ -18,16 +18,16 @@ const SearchBar: React.FC<any> = () => {
   }, [pathname]);
 
   return (
-    <Flex p="16px" position="relative" alignItems="center">
+    <Flex pb={6} position="relative" alignItems="center">
       <form onSubmit={goToSearch} style={{ width: "100%" }}>
         <Input
           paddingLeft="50px"
-          value={searchValue}
+          value={searchValue || searchParam}
           onChange={(e) => setSearchValue(e.target.value)}
           placeholder="Search"
         />
       </form>
-      <Box position="absolute" left="30px" color="gray">
+      <Box position="absolute" left="20px" color="gray">
         <AiOutlineSearch size="20px" />
       </Box>
     </Flex>
