515b112d5b50b7f687c61836d0b7c1b821744c18
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2022-08-26 21:08:12 +0200

    improve: get details data in the Home page, instead of in Details only

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,10 +4,11 @@ import { api } from "../../services/api";
 import { useStore } from "../../services/store";
 import { ResponseData } from "../../types";
 import { Cards, Pagination, SearchBar, SideCart } from "../../components";
+import { getIdFromUrl } from "../../utils/urls";
 
 const Home = () => {
   const [isCardLoading, setIsCardLoading] = useState<Boolean>(false);
-
+  const [completeData, setCompleteData] = useState<any>([]);
   const {
     currentPage,
     setDisplayPokemons,
@@ -15,20 +16,27 @@ const Home = () => {
     setCurrentNumberOfPokemons,
   } = useStore();
 
-  const setPokemons = (response: ResponseData) => {
-    setTotalNumberOfPokemons(response.count);
-    setCurrentNumberOfPokemons(response.count);
-    setDisplayPokemons(response.results);
+  const getDetailedData = async (
+    basicData: { name: string; url: string }[]
+  ) => {
+    return Promise.all(
+      basicData.map(async (pokemon: { name: string; url: string }) => {
+        const details = await api.get(`/pokemon/${getIdFromUrl(pokemon.url)}`);
+        return { ...pokemon, ...details.data };
+      })
+    );
   };
 
   useEffect(() => {
     const getPokemons = async () => {
       setIsCardLoading(true);
       try {
-        const response = await api.get(
+        const basicData = await api.get(
           `/pokemon?offset=${currentPage * 20}&limit=20`
         );
-        setPokemons(response.data);
+        setTotalNumberOfPokemons(basicData.data.count);
+        setCurrentNumberOfPokemons(basicData.data.count);
+        setDisplayPokemons(await getDetailedData(basicData.data.results));
       } catch (error) {
         console.error(error);
       } finally {
